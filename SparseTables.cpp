eb250ce13400d692f57473319d03b10e628148b9
Zeyad Ammar <168654573+Zeyad-Ammar@users.noreply.github.com>
2026-04-20 01:26:03 +0200

    Add SparseTable class for efficient range queries
    Implement Sparse Table data structure for range queries.

---
+++ SparseTables.cpp
@@ -0,0 +1,64 @@
+struct SparseTable {
+
+	vvi v;
+	int n=1;
+	int size=0;
+	int ign=-oo;
+
+	SparseTable(vi &vec) {
+
+		size=sz(vec);
+		n=__lg(size);
+		v=vvi(n+1,vi(size));
+		v[0]=vec;
+
+		build();
+
+	}
+
+	int merge(int a, int b) {
+		ret max(a,b);
+	}
+
+	void build() {
+
+
+		for (int len=1;len<=n;len++) {
+			int sl=len-1;
+
+			for (int i=0;i<size-(1<<len)+1;i++) {
+				int l=v[sl][i];
+				int r=v[sl][i+(1<<sl)];
+
+				v[len][i]=merge(l,r);
+			}
+
+		}
+	}
+
+	bool isOn(int num,int bit) {
+		return (num>>bit)&1;
+	}
+
+	int query(int l,int r) {
+		int len=r-l+1;
+		int ans=ign;
+		for (int b=0;l<=r;b++) {
+			if (isOn(len,b)) {
+
+				ans=merge(ans,v[b][l]);
+				l+=(1<<b);
+			}
+		}
+
+		return ans;
+	}
+
+	int queryOL(int l,int r) {
+		int len=r-l+1;
+		int pow=__lg(len);
+
+		return merge(v[pow][l],v[pow][r-(1<<pow)+1]);
+	}
+
+};
