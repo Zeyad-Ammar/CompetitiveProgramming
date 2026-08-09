b5828e5028804d82c29c91b4cb47b7240a5f8929
Zeyad Ammar <168654573+Zeyad-Ammar@users.noreply.github.com>
2026-08-10 00:15:56 +0300

    Add linear sieve function for prime number generation
    Implement linear sieve algorithm to find prime numbers.

---
+++ sieve.cpp
@@ -0,0 +1,17 @@
+const int N=1e5+5;
+vector<bool>isPrime(N,true);//can use it to check if a number less than N is prime or not
+vector<int>p;//have all prime numbers less than N.
+void linear_sieve()
+{
+    isPrime[0]=isPrime[1]=0;
+    for(ll i=2;i<N;i++)
+    {
+        if(isPrime[i]==1) p.push_back(i);
+        for(ll o=0;o<p.size();o++)
+        {
+            if(i*p[o]>=N) break;
+            isPrime[i*p[o]]=0;
+            if(i%p[o]==0) break;
+        }
+    }
+}
