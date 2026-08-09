4f96e34078dd4e3b2ef0a3afc4ab8326c293d8e2
Zeyad Ammar <168654573+Zeyad-Ammar@users.noreply.github.com>
2026-08-09 23:40:46 +0300

    Add function to compute number of divisors
    Implement a function to calculate the number of divisors for large numbers using prime factorization.

---
+++ numberOfDivisors.cpp
@@ -0,0 +1,47 @@
+//this function give u the number of divisors of any number even if it's 2e18 without time limit
+//in fast time but the number of them only but not them.
+//to get them u will need to use sqrt(n) algorithm to get them.
+ll numberOfDivisors(ll n)
+{
+    int primes[]={2,3,5,7,11,13,17,19};
+    ll num=1,ans=1;
+    for (int it:primes)
+    {
+        int c=0;
+        while(n%it==0)
+        {
+            n/=it;
+            c++;
+        }
+        ans*=c+1;
+        num*=it;
+    }
+    int all=0;
+    for(int i=1;i<num;i++)
+    {
+        bool can=1;
+        for (int it:primes)
+        {
+            if(i%it==0)
+            {
+                can=0;
+            }
+        }
+        if(can)
+        {
+            ll o=i;
+            for (;o*o<n;o+=num)
+            {
+                if(n%o==0)
+                {
+                    all += 2;
+                }
+            }
+            if (o*o==n)
+            {
+                all++;
+            }
+        }
+    }
+    return ans*all;
+}
