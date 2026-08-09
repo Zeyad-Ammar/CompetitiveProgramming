ac293b7e54d3e0fe051d876c22814702270d1af9
Zeyad Ammar <168654573+Zeyad-Ammar@users.noreply.github.com>
2026-08-09 23:00:50 +0300

    Clarify cost vector purpose in dijkstra.cpp
    Updated the cost vector description for clarity.

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -4,7 +4,7 @@
 //to all other reachable nodes (or a specific destination node).
 const int N=1e5+5;
 vector<pair<int,int>>adj[N];//the graph representation
-vector<int>cost(N,-1);//the vector that U will use to find the shortest or lowest total cost from ur start node.
+vector<int>cost(N,-1);//the vector that U will use to find the shortest or lowest total cost from ur start node and i node.
 void dijkstra(int start)//pass the root node
 {
     priority_queue<pair<int,int>,deque<pair<int,int>>,greater<pair<int,int>>>pq;
