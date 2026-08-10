86f248d729abbe7663d596cb2a4bc16caf5ad6cd
Zeyad Ammar <168654573+Zeyad-Ammar@users.noreply.github.com>
2026-08-11 01:32:39 +0300

    Add KMP algorithm implementation in KMP.cpp
    Implement KMP algorithm for pattern searching in strings.

---
+++ KMP.cpp
@@ -0,0 +1,25 @@
+const int N=1e5+5;
+struct KMP{//can give u ans vector that have all index that this pattern appeared.
+    int longestPrefix[N]={};vector<int>ans;
+    void calcPrefix(string patern)
+    {
+        int n=patern.size();
+        for(int i=1,idx=0;i<n;i++)
+        {
+            while(idx>0&&patern[idx]!=patern[i])idx=longestPrefix[idx-1];
+            if(patern[i]==patern[idx])idx++;
+            longestPrefix[i]=idx;
+        }
+    }
+    void kmp(string s,string pat)//pass the string and the pattern u want to search about.
+    {
+        int n=s.size(),m=pat.size();
+        calcPrefix(pat);
+        for(int i=0,idx=0;i<n;i++)
+        {
+            while(idx>0&&s[i]!=pat[idx])idx=longestPrefix[idx-1];
+            if(s[i]==pat[idx])idx++;
+            if(idx==m)ans.push_back(i-m+1),idx=longestPrefix[idx-1];
+        }
+    }
+};
